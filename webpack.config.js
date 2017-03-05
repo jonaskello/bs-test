7253f2f3f2920a7e64ec0b95ee1b5336749de159
Jonas Kello <jonas.kello@divid.se>
2017-03-05 23:22:34 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/root.re',
+  output: {
+    filename: 'out.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      { test: /.(re|ml)$/, use: 'bs-loader' },
+    ]
+  },
+  resolve: {
+    extensions: ['.re', '.ml', '.js']
+  }
+}
